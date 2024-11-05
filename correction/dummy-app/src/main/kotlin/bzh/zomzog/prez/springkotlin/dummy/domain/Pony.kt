17636b4417006c7bfc7c9f0abe2d72294ccc99d0
Zomzog <zomzog@gmail.com>
2024-11-05 01:04:28 +0100

    clean

--- correction/dummy-app/src/main/kotlin/bzh/zomzog/prez/springkotlin/dummy/domain/Pony.kt
+++
@@ -1,9 +0,0 @@
-package bzh.zomzog.prez.springkotlin.dummy.domain
-
-data class Pony(val id: Int?, val name: String, val type: PonyType)
-
-enum class PonyType {
-    EARTH,
-    PEGASIS,
-    UNICORN
-}
--- correction/user-app/src/main/kotlin/bzh/zomzog/prez/springkotlin/domain/User.kt
+++
@@ -1,3 +0,0 @@
-package bzh.zomzog.prez.springkotlin.domain
-
-data class User(val email: String, val firstName: String, val lastName: String, val age: Int)
